da397c68683acb5ad778888054c7af893b679621
Julian Cardenas <julian.cardenas@definityfirst.com>
2018-10-31 15:55:52 -0600

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import initialData from "./initial-data";
+import Column from "./Component/Column"
+import {DragDropContext} from 'react-beautiful-dnd';
+import styled from 'styled-components';
+const Container = styled.div`
+display:
+`;
+
 
 class App extends Component {
+  state = initialData;
+  
+  onDragEnd = (result) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor="inherit";
+    const {destination , source , draggableId} = result;
+
+    if(!destination || (destination.droppableId === source.droppableId && destination.index === source.index))   return;
+    
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index,1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+   
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,     
+         [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
+  };
+
+  onDragStart = () =>{
+document.body.style.color ='orange';
+document.body.style.transition = 'background-color 0.2s ease';
+  }
+
+  onDragUpdate = (update) =>{
+    const {destination} = update;
+    const opacity = destination ? destination.index / Object.keys(this.state.tasks).length : 0;
+    document.body.style.backgroundColor = `rgba( 153, 141, 217, ${opacity})`;
+  }
+    
+
+   renderDragContext  = () => {
+let jsx = [];
+jsx.push(<div>
+    <DragDropContext
+     onDragEnd ={this.onDragEnd}
+     onDragUpdate={this.onDragUpdate}
+     onDragStart={this.onDragStart}>
+      <Container>
+       {this.state.columnOrder.map(columnId => {
+       const column = this.state.columns[columnId];
+       const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+       return <Column key={column.id} column={column} tasks={tasks}></Column> })}
+         </Container>
+     </DragDropContext>
+
+     </div>
+    );
+return jsx;
+  }
+ 
+ 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+
+    return (<div>
+{this.renderDragContext()}
+
+    </div>
+   
+
+   
     );
   }
+  
 }
 
 export default App;
